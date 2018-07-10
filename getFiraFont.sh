e534c47da46ba63dad462a7ac83a06f68f729f4c
Josef Eisl <josef.eisl@jku.at>
2018-07-10 18:56:08 +0200

    Harden the getFiraFont script

--- getFiraFont.sh
+++ getFiraFont.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e
+
 ## cf from http://programster.blogspot.com/2014/05/ubuntu-14-desktop-install-fira-sans-and.html
 
 cd /tmp
 
 # install unzip just in case the user doesn't already have it.
-apt-get install unzip -y
+#apt-get install unzip -y
 
 # Fonts also available at: https://github.com/bBoxType/FiraSans
 wget "https://bboxtype.com/downloads/Fira/Download_Folder_FiraSans_4301.zip"
@@ -14,8 +16,8 @@ wget "https://bboxtype.com/downloads/Fira/Fira_Mono_3_2.zip"
 unzip Download_Folder_FiraSans_4301.zip
 unzip Fira_Mono_3_2.zip
 
-sudo mkdir -p /usr/share/fonts/truetype/FiraSans
-sudo mkdir -p /usr/share/fonts/opentype/FiraSans
+mkdir -p /usr/share/fonts/truetype/FiraSans
+mkdir -p /usr/share/fonts/opentype/FiraSans
 
 cp Download_Folder_FiraSans_4301/Fonts/Fira_Sans_TTF_4301/*/*/*.ttf \
 	/usr/share/fonts/truetype/FiraSans/
