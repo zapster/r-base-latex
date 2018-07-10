cf1db5f041c9a639ddc7e9a834d7f146c09e5559
Josef Eisl <zapster@zapster.cc>
2018-07-10 18:08:32 +0200

    Update getFiraFont.sh

--- getFiraFont.sh
+++ getFiraFont.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 ## cf from http://programster.blogspot.com/2014/05/ubuntu-14-desktop-install-fira-sans-and.html
-## from https://github.com/matze/mtheme
 
-WORKDIR=/tmp/fira
+cd /tmp
 
-mkdir -p $WORKDIR
-cd $WORKDIR
+# install unzip just in case the user doesn't already have it.
+apt-get install unzip -y
 
-wget "http://www.carrois.com/downloads/fira_4_1/FiraFonts4106.zip"
-wget "http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip"
+# Fonts also available at: https://github.com/bBoxType/FiraSans
+wget "https://bboxtype.com/downloads/Fira/Download_Folder_FiraSans_4301.zip"
+wget "https://bboxtype.com/downloads/Fira/Fira_Mono_3_2.zip"
 
-unzip FiraFonts4106.zip
-unzip FiraMonoFonts3206.zip
+unzip Download_Folder_FiraSans_4301.zip
+unzip Fira_Mono_3_2.zip
 
-mkdir -p /usr/share/fonts/truetype/FiraSans
-mkdir -p /usr/share/fonts/opentype/FiraSans
-cp Fira*/WEB/TTF/*.ttf /usr/share/fonts/truetype/FiraSans/
-cp Fira*/OTF/Fira* /usr/share/fonts/opentype/FiraSans/
+sudo mkdir -p /usr/share/fonts/truetype/FiraSans
+sudo mkdir -p /usr/share/fonts/opentype/FiraSans
+
+cp Download_Folder_FiraSans_4301/Fonts/Fira_Sans_TTF_4301/*/*/*.ttf \
+	/usr/share/fonts/truetype/FiraSans/
+cp Download_Folder_FiraSans_4301/Fonts/Fira_Sans_OTF_4301/*/*/*.otf \
+	/usr/share/fonts/opentype/FiraSans/
+cp Fira_Mono_3_2/Fonts/FiraMono_WEB_32/*.ttf /usr/share/fonts/truetype/FiraSans
+cp Fira_Mono_3_2/Fonts/FiraMono_OTF_32/*.otf /usr/share/fonts/truetype/FiraSans
+
+rm Download_Folder_FiraSans_4301.zip Fira_Mono_3_2.zip
+rm -rf Download_Folder_FiraSans_4301 Fira_Mono_3_2
 
 fc-cache -fv
-rm -rf $WORKDIR
